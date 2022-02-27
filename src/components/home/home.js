a7b8024805ebde5e783991c34fdf32ce0486dea8
marc <marcdownie3@hotmail.com>
2022-02-27 14:50:11 +0000

    home screen fade in

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,26 +1,32 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import '../home/home.css'
 import marcHead from '../../assets/sam_jack.jpg'
+import Aos from 'aos'
+import 'aos/dist/aos.css'
 
 
 
 export default function Home() {
+  useEffect(() => {
+    Aos.init({})
+}, []);
+
   return (
     <>
       <div className="home-bg">
 
-        <div className='name-wrapper'>
+        <div data-aos-duration="1500" data-aos="fade" className='name-wrapper'>
           <text className="name">Marc Downie</text>
           <text className="job-title">Software Engineer</text>
         </div>
 
-        <div className="img-wrapper">
+        <div data-aos-duration="2000" data-aos="fade" className="img-wrapper">
           <img className="img-head" src={marcHead} alt='My Face'></img>
         </div>
 
-        <text className="home-txt">
+        <div data-aos-duration="2000" data-aos="fade" className="home-txt">
           <h3>Welcome</h3>
-        </text>
+        </div>
       </div>
     </>
   )
